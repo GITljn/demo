bd95a7230d828c0dbde97edfb5fd1660f780dfc9
liujianing <742277203@qq.com>
2022-08-30 15:24:34 +0800

    修改aop包中的内容

--- src/main/java/com/ljn/demo/aop/aop_test/aspect/MyAspectAround.java
+++ src/main/java/com/ljn/demo/aop/aop_test/aspect/MyAspectAround.java
@@ -7,13 +7,10 @@ import org.aspectj.lang.annotation.Pointcut;
 //@Component
 //@Aspect
 public class MyAspectAround {
-
 //    用Pointcut注解中的属性(切点表达式或者切点函数)来指定对哪些方法进行增强，该注解必须用在方法上
     @Pointcut("execution(* com.ljn.demo.aop.aop_test.service.ServiceAround.*(..))")
     public void pt(){}
 
-//    用@Before注解来指定该方法中是增强的代码，并且是在被增强方法执行前执行的
-//    @Before的属性写上加了@Pointcut注解的方法: 方法名()
     @Around("pt()")
     public void func(ProceedingJoinPoint pjp){
         System.out.println("目标方法前执行");
--- src/main/java/com/ljn/demo/aop/aop_test/service/ServiceExecution.java
+++ src/main/java/com/ljn/demo/aop/aop_test/service/ServiceExecution.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ServiceExecution {
-
-
     public void deleteAll(){
         System.out.println("ServiceExecution中deleteAll的核心代码");
     }
