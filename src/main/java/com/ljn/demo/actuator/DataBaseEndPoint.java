01a99fd5e859d6cb805b2c047f26922e10a89bc5
ljn <742277203@qq.com>
2022-10-23 13:12:42 +0800

    增加项目监控Acturator

---
+++ src/main/java/com/ljn/demo/actuator/DataBaseEndPoint.java
@@ -0,0 +1,32 @@
+package com.ljn.demo.actuator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+// 监控数据库的端点
+@Component
+// id为端点的名称
+// 访问路径: ip:port/context path/acturator/database
+@Endpoint(id = "database")
+public class DataBaseEndPoint {
+    @Autowired
+    private DataSource dataSource;
+
+    // 接收get请求
+    @ReadOperation
+    @ResponseBody
+    public R testConnection() {
+        try (Connection connection = dataSource.getConnection()) {
+            return R.success().msg("数据库连接成功");
+        } catch (SQLException throwables) {
+            return R.error().msg("数据库连接失败");
+        }
+    }
+}
