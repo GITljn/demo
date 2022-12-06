01304e813109a150b99473e79016078a6ae1665c
刘佳宁 <8782095+giteeljn@user.noreply.gitee.com>
2022-12-06 21:09:52 +0800

    增加发送邮件工具类

---
+++ src/main/java/com/ljn/demo/utils/MailUtil.java
@@ -0,0 +1,46 @@
+package com.ljn.demo.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Component
+public class MailUtil {
+    private static final Logger logger = LoggerFactory.getLogger(MailUtil.class);
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    public void sendMail(String from, String to, String subject, String text, boolean html) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, html);
+            mailSender.send(helper.getMimeMessage());
+        } catch (MessagingException e) {
+            logger.error("发送邮件失败: " + e.getMessage());
+        }
+    }
+
+    public void demo() {
+        Context context = new Context();
+        context.setVariable("email", "xxx@xxx.com");
+        context.setVariable("url", "xxxx/activation/userId/activateCode");
+        String content = templateEngine.process("/mail/activation", context);
+        sendMail("from", "to", "subject", content, true);
+    }
+}
